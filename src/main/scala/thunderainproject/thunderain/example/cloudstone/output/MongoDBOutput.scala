532d5c3e14b93bafced4537cdbb15263e02700db
Grace <jie.huang@intel.com>
2013-11-13 13:50:49 +0800

    Make MongoDBClient as a singleton to reduce the connection number to the db server

--- src/main/scala/thunderainproject/thunderain/example/cloudstone/output/MongoDBOutput.scala
+++ src/main/scala/thunderainproject/thunderain/example/cloudstone/output/MongoDBOutput.scala
@@ -4,14 +4,27 @@ import thunderainproject.thunderain.framework.output.{PrimitiveObjInspectorFacto
 import thunderainproject.thunderain.framework.Event
 
 import org.apache.spark.streaming.DStream
+import org.apache.spark.Logging
 
 import com.mongodb.casbah.Imports._
 import scala.collection.mutable
 import scala.transient
 import java.net.{HttpURLConnection, URL}
 
+//singleton for mongoDB client which contains a connection pool (10 threads by default)
+object MongoDBOutput{
+  private var mongoDBClientOnSlave: MongoClient = _
+  def getClientOnSlave(address: String): MongoClient = synchronized {
+    if(mongoDBClientOnSlave == null) {
+      val mongoURL  = MongoClientURI(address)
+      mongoDBClientOnSlave = MongoClient(mongoURL)
+    }
+    //println("client:" + mongoDBClientOnSlave)
+    mongoDBClientOnSlave
+  }
+}
 
-class MongoDBOutput extends AbstractEventOutput{
+class MongoDBOutput extends AbstractEventOutput with Logging{
   //TODO to load the outputFormat automatically from shark/hive tableInfo
   @transient lazy val outputFormat = mutable.LinkedHashMap (
     ("h_host_ip", "String"),
@@ -37,22 +50,28 @@ class MongoDBOutput extends AbstractEventOutput{
 
   val mongoDBupdatesTrigger = System.getenv("MONGO_DB_UPDATES_TRIGGER")
 
-  @transient lazy val mongoURL = MongoClientURI(mongoAddress)
-  @transient lazy val mongoDBClientOnSlave = MongoClient(mongoURL).apply(mongoDB)
-  @transient lazy val table = mongoDBClientOnSlave(outputName)
-
   def output(stream: DStream[_]): Unit = {
     //insert each row into mongoDBCollection
     stream.foreach(r => {
       val filteredRDD = r.filter(_.asInstanceOf[Event].keyMap.nonEmpty)
-      filteredRDD.foreach(row => {
-        val cells = new mutable.HashMap[String, Any]
-        row.asInstanceOf[Event].keyMap.map(col => {
-          cells(col._1) = PrimitiveObjInspectorFactory.stringObjConversion(col._2, outputFormat(col._1))
-        })
-        table.insert(MongoDBObject(cells.toList))
-        //TODO find alternative way
-        //table.ensureIndex({"h_time"->"hashed"})
+      filteredRDD.foreachPartition((iter: Iterator[Any]) => {
+        val db = MongoDBOutput.getClientOnSlave(mongoAddress).apply(mongoDB)
+        val table = db(outputName)
+        try {
+          iter.foreach( row => {
+            val cells = new mutable.HashMap[String, Any]
+            row.asInstanceOf[Event].keyMap.map(col => {
+              cells(col._1) = PrimitiveObjInspectorFactory.stringObjConversion(col._2, outputFormat(col._1))
+            })
+            //must have the WriteConcern option to ensure the performance
+            table.insert(MongoDBObject(cells.toList), WriteConcern.None)
+          })
+          table.ensureIndex(MongoDBObject("h_time" -> 1))
+          table.ensureIndex(MongoDBObject("h_time" -> -1))
+        }
+        catch {
+          case _ => logError("Failed to insert DB")
+        }
       })
 
       if(mongoDBupdatesTrigger != null) {
@@ -60,7 +79,7 @@ class MongoDBOutput extends AbstractEventOutput{
         if(recordCnt > 0){
           val url = "%s/%d".format(mongoDBupdatesTrigger, recordCnt)
           if(sendHTTPGetRequest(url) != 200)
-            throw new Exception("Bad ack from %s".format(url))
+            logError("Bad ack from %s".format(url))
         }
       }
     })
@@ -73,3 +92,4 @@ class MongoDBOutput extends AbstractEventOutput{
     con.getResponseCode()
   }
 }
+
